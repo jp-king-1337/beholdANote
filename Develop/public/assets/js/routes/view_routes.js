f15b0c02579117b7f0a2c62269fff3a7dea7ee93
JP <134244890+jp-king-1337@users.noreply.github.com>
2023-07-07 23:33:22 -0400

    Update view_routes.js
    Had to update on GitHub because git push wasn't working for some reason. Kept demanding that I git pull, but every time I ran git pull it told me it couldn't pull the branch.

--- Develop/public/assets/js/routes/view_routes.js
+++ Develop/public/assets/js/routes/view_routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const path = require("path");
+const fs = require("fs");
 
+const dbPath = path.join(__dirname, "../../../db/db.json")
 
 // HTML routes
 // Module 11 on BCS says that "GET * should return the index.html file." but the asterisk just breaks the button. With * on line 5, the button directs to index.html. With / on line 5, the button works properly along with clicks on the Note Taker in the nav bar. So I'm leaving it as / instead of *.
@@ -13,7 +15,15 @@ router.get("/notes", (clientReq, serverRes) => {
 });
 
 // API routes
-// router.get
+router.get("/api/notes", (clientReq, serverRes) => {
+    fs.readFile(dbPath, "utf8", (err, data) => {
+        if (err) throw err;
+
+        let notes = JSON.parse(data);
+
+        serverRes.json(notes);
+    });
+});
 
 // router.post
 
