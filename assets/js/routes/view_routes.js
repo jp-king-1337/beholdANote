0555073c98082565902d1be883b475ffce759744
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-08 01:58:29 -0400

    Got landing page up on GitHub deployed link, working on getting the notes page there too

--- assets/js/routes/view_routes.js
+++ assets/js/routes/view_routes.js
@@ -4,16 +4,16 @@ const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
-const dbPath = path.join(__dirname, "../../../../db/db.json")
+const dbPath = path.join(__dirname, "../../../db/db.json")
 
 // HTML routes
 // Module 11 on BCS says that "GET * should return the index.html file." but the asterisk just breaks the button. With * on line 5, the button directs to index.html. With / on line 5, the button works properly along with clicks on the Note Taker in the nav bar. So I'm leaving it as / instead of *.
 router.get("/", (clientReq, serverRes) => {
-    serverRes.sendFile(path.join(__dirname, "../../../index.html"));
+    serverRes.sendFile(path.join(__dirname, "../../index.html"));
 });
 
 router.get("/notes", (clientReq, serverRes) => {
-    serverRes.sendFile(path.join(__dirname, "../../../notes.html"));
+    serverRes.sendFile(path.join(__dirname, "../../notes.html"));
 });
 
 // API routes
@@ -57,11 +57,9 @@ router.delete("/api/notes/:id", (clientReq, serverRes) => {
 
         let notes = JSON.parse(data);
 
-        // Need to find the note with the given id
         const noteIndex = notes.findIndex((note) => note.id === noteId);
 
         notes.splice(noteIndex, 1);
-        // I think splice will work to delete? If I splice it at 1 it should take away every part of the selected note
 
         fs.writeFile(dbPath, JSON.stringify(notes), (err) => {
             if (err) throw err;
