981057fd51e8924bad91c2db89efb7e4057a39bc
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-07 22:53:01 -0400

    I have HTML routes for /notes and for / now

--- Develop/public/assets/js/routes/view_routes.js
+++ Develop/public/assets/js/routes/view_routes.js
@@ -1,6 +1,11 @@
 const router = require("express").Router();
 const path = require("path");
 
+// Module 11 on BCS says that "GET * should return the index.html file." but the asterisk just breaks the button. With * on line 5, the button directs to index.html. With / on line 5, the button works properly along with clicks on the Note Taker in the nav bar. So I'm leaving it as / instead of *.
+router.get("/", (clientReq, serverRes) => {
+    serverRes.sendFile(path.join(__dirname, "../../../index.html"));
+});
+
 router.get("/notes", (clientReq, serverRes) => {
     serverRes.sendFile(path.join(__dirname, "../../../notes.html"));
 });
