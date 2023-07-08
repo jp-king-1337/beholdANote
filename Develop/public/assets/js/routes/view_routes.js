43e0102cf6010fcfd3fb5cd4ca567fdd398c32da
Rabbit <134244890+jp-king-1337@users.noreply.github.com>
2023-07-08 00:01:03 -0400

    Had an error on view_routes.js line 7. dbPath needed an additional ../ but now it works

--- Develop/public/assets/js/routes/view_routes.js
+++ Develop/public/assets/js/routes/view_routes.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const path = require("path");
 const { v4: uuidv4 } = require("uuid");
 
-const dbPath = path.join(__dirname, "../../../db/db.json")
+const dbPath = path.join(__dirname, "../../../../db/db.json")
 
 // HTML routes
 // Module 11 on BCS says that "GET * should return the index.html file." but the asterisk just breaks the button. With * on line 5, the button directs to index.html. With / on line 5, the button works properly along with clicks on the Note Taker in the nav bar. So I'm leaving it as / instead of *.
